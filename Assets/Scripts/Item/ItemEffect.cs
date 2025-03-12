dcac18ce67e6b13bfef7d2d075383cfcd601ee06
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-03-12 19:17:44 +0900

    Feat: Item Effect Update

--- Assets/Scripts/Item/ItemEffect.cs
+++ Assets/Scripts/Item/ItemEffect.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ItemEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject itemEffectPrefab;
+
+    public float destroyDelay = 0.1f;
+    private bool isDestroyed;
+
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player") && !isDestroyed)
+        {
+
+            PlayerAction player = collision.GetComponent<PlayerAction>();
+
+            if (player != null)
+            {
+                GameObject effectInstance = PlayEffect();
+
+                if (effectInstance != null)
+                {
+                    Destroy(effectInstance, destroyDelay);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject PlayEffect()
     {
-        
+        if (itemEffectPrefab != null)
+        {
+            GameObject effectInstance = Instantiate(itemEffectPrefab, transform.position, Quaternion.identity);
+            return effectInstance;
+        }
+        return null;
     }
+    */
 }
