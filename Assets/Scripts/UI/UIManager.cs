129aacb5800f8999ddfe7d53229ef61253562757
buildthan <a9175226@gmail.com>
2025-03-12 14:06:25 +0900

    [ Feat ] UI 스크립트 추가

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+using System;
+using System.Collections;
+
+public enum UIState
+{
+    Title, //0
+    Lobby, //1
+    Game, //2
+}
+
+public class UIManager : MonoBehaviour
+{
+    UIState currentState = UIState.Title;
+
+    TitleUI titleUI = null;
+    LobbyUI lobbyUI = null;
+    GameUI gameUI = null;
+
+    static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(this.gameObject);
+        }
+
+
+        titleUI = GetComponentInChildren<TitleUI>(true);
+        titleUI?.Init(this);
+        lobbyUI = GetComponentInChildren<LobbyUI>(true);
+        lobbyUI?.Init(this);
+        gameUI = GetComponentInChildren<GameUI>(true);
+        gameUI?.Init(this);
+
+        ChangeState(UIState.Title);
+    }
+
+
+    public void ChangeState(UIState state) //UI오브젝트를 on off 해주는 기능
+    {
+        currentState = state; //아래에서 해당하는 UI오브젝트를 찾아 on off 해줌
+        titleUI?.SetActive(currentState);
+        lobbyUI?.SetActive(currentState);
+        gameUI?.SetActive(currentState);
+    }
+
+
+
+}
