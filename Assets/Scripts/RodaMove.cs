b7bd947ee37c532b57d1dc70bc78f3e03b990f1d
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-03-12 13:47:09 +0900

    [feat] 2D TileMap 설치

---
+++ Assets/Scripts/RodaMove.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float roadSpeed;
+    private Vector3 moveVec;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+ 
+    }
+}
