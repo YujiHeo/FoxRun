3c74a34f9500ccca7b18cb6079769f7283c6ca60
Kim-giseok <belbeck@naver.com>
2025-03-12 16:46:06 +0900

    Feat: 카메라 추적
    플레이어가 생성되면 메인카메라가 플레이어를 따라다니도록 합니다.
    플레이어가 삭제되면 메인카메라는 더이상 플레이어를 따라다니지 않습니다.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,11 +6,28 @@ public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+    public Transform cameraPoint;
+
+    Camera mainCamera;
 
     void Awake()
     {
         GameManager.instance.player = this;
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
+
+        mainCamera = Camera.main;
+        if (mainCamera != null && cameraPoint != null)
+        {
+            mainCamera.transform.parent = cameraPoint;
+            mainCamera.transform.localPosition = Vector3.zero;
+            mainCamera.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (mainCamera != null)
+            mainCamera.transform.parent = null;
     }
 }
