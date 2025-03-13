6bfda5250ee315e6c5448623922000fedc0442b2
Kim-giseok <belbeck@naver.com>
2025-03-13 16:08:12 +0900

    [Feat] 업적 관리자 업적 정보들 저장&업적 달성도 관리 구현

--- Assets/Scripts/Achivement/AchivementManager.cs
+++ Assets/Scripts/Achivement/AchivementManager.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class AchivementManager : MonoBehaviour
 {
+    static AchivementManager _instance;
+    public static AchivementManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new GameObject("AchivementManager").AddComponent<AchivementManager>();
+            return _instance;
+        }
+    }
+
     [SerializeField] AchivData[] achivs;
 
     void Awake()
     {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         achivs = Resources.LoadAll<AchivData>("Achivements");
+#if UNITY_EDITOR
+        foreach (var achiv in achivs)
+            achiv.currnetCount = 0;
+#endif
+    }
+
+    public void SignAchivement(int idx, int addCount)
+    {
+        achivs[idx].UpdateAchiv(addCount);
     }
 }
--- Assets/Scripts/Achivement/AhivData.cs
+++ Assets/Scripts/Achivement/AhivData.cs
@@ -8,5 +8,16 @@ public class AchivData : ScriptableObject
     public string achivName;
     public string description;
     public Sprite icon;
-    public bool isClear = false;
+    public int goalCount;
+    public int currnetCount;
+    bool isClear => currnetCount >= goalCount;
+
+    public void UpdateAchiv(int addCount)
+    {
+        if (isClear) return;
+
+        currnetCount += addCount;
+        if (isClear)
+            Debug.Log(achivName + "´Þ¼º");
+    }
 }
