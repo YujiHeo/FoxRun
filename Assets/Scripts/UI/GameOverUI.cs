3a4973a7ccf51019fff67a5a1290bfd59299955c
buildthan <a9175226@gmail.com>
2025-03-14 20:04:48 +0900

    Revert "[Fix]로비로 복귀하지 않는 현상 수정"
    This reverts commit b9dabe34f8d852442fc81cb76998f9de34e747ad.

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -16,8 +16,6 @@ public class GameOverUI : BaseUI
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
-
-        lobbyButton.onClick.AddListener(OnClickGameOverLobbyButton);
     }
 
     public void SetScoreText()
