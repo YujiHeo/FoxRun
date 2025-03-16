6ea11a3f505da344a645541d0ffd8471eea7f1b9
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-03-16 21:41:18 +0900

    [test] 마그네틱 수정 버전 작성

---
+++ Assets/Scripts/Maps/MagneticRevision.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticRevision : MonoBehaviour
+{
+    //플레이어가 아니라 아이템을 인식해서 끌어당기게....
+
+    public float moveSpeed = 10f;
+    public float magnetDistance = 30f;
+    public GameObject items;
+
+    private List<GameObject> itemsList = new List<GameObject>();
+    private GameObject child;
+    private int childCount;
+
+
+    void Update()
+    {
+        itemsList.Clear();
+
+        childCount = items.transform.childCount;
+
+        for (int i = 0; i < childCount; i++ )
+        {
+            child = items.transform.GetChild(i).gameObject;
+
+            if(child.activeSelf)
+            {
+                itemsList.Add(child);
+            }
+        }
+
+        ApplyMagnet();
+    }
+
+    public void ApplyMagnet()
+    {
+        foreach (GameObject item in itemsList)
+        {
+            if (PlayerCondition.isMagnet && item != null)
+            {
+                float distance = Vector3.Distance(transform.position, item.transform.position);
+
+                if (distance <= magnetDistance)
+                {
+                    item.transform.position = Vector3.MoveTowards(item.transform.position, transform.position, moveSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
